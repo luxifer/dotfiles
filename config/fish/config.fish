c4807c2b32be638b15bfe40b19a7a39cd2d9ee15
Florent Viel <fviel@scaleway.com>
2025-10-09 11:02:45 +0200

    update conf

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,11 +8,13 @@ set -Ux REQUESTS_CA_BUNDLE /etc/ssl/certs/ca-certificates.crt
 set -Ux STOPIT 1
 set -Ux SHUTUP 1
 set -Ux GOAWAY 1
+fish_add_path /usr/local/go/bin
 fish_add_path $GOPATH/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.pulumi/bin
 fish_add_path ~/.poetry/bin
 fish_add_path ~/.krew/bin
+fish_add_path ~/.cargi/bin
 
 starship init fish | source
 direnv hook fish | source
